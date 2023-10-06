7d433dc8f1979922baa6adbe3f547feae2ef27c6
Andrew-Likess <a.dzyubko200@gmail.com>
2023-10-06 20:38:38 +0300

    bag in link login

---
+++ project/main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    data = {
+        'title': 'Главная страница',
+
+    }
+    return render(request, 'main/index.html', data)
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def contact(request):
+    return render(request, 'main/contact.html')
