d711e65460af7789627c3bdde658abb6413e6190
Andrew-Likess <a.dzyubko200@gmail.com>
2023-10-10 22:43:25 +0300

    changet admin in app users

--- project/users/admin.py
+++ project/users/admin.py
@@ -1,30 +1,10 @@
-from django.shortcuts import render, redirect
-from django.views import View
-from django.contrib.auth import authenticate, login
-from users.forms import UserCreationForm
-# Create your views here.
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+User = get_user_model()
 
 
-class Register(View):
-    template_name = 'users/registration/register.html'
-
-    def get(self, request):
-        context = {
-            'form': UserCreationForm()
-        }
-        return render(request, self.template_name, context)
-
-    def post(self, request):
-        form = UserCreationForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('user')
-        context = {
-            'form': form
-        }
-        return render(request, self.template_name, context)
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    pass
